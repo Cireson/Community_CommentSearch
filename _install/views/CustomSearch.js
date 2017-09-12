5d2bb096f0789cf7044b8e867b65441b7b359de3
LAB\scoussens <seth.coussens@gmail.com>
2017-09-12 09:29:12 -0500

    added view panels to install

---
+++ _install/views/CustomSearch.js
@@ -0,0 +1,20 @@
+{
+    "Id": "CustomSearch.js",
+    "layoutType": "semantic",
+    "pageTitle": "",
+    "view": {
+        "cssClass": "container-fluid",
+        "content": [
+            {
+                "cssClass": "row",
+                "content": [
+					{
+						"cssClass": "col-md-12",
+						"type": "viewPanel",
+						"ViewPanelId": "CustomSearch-vp.js"
+					}
+                ]
+            }
+        ]
+    }
+}
---
+++ _install/views/viewpanels/CustomSearch-vp.js
@@ -0,0 +1,7 @@
+{
+    "Id": "CustomSearch-vp",
+    "TypeId": "html",
+    "Definition": {
+        "content": "<div id='CustomSearch'></div><script>$('#CustomSearch').load('/CustomSpace/CustomSearch/index.html');</script>"
+    }
+}
